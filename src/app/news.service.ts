4164bf92fd53665b21424c50f6410a5af6ff9962
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-02-22 11:57:20 +0200

    Save apiKey in a property in news service

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -8,12 +8,14 @@ import { News } from './news.model';
   providedIn: 'root',
 })
 export class NewsService {
+  apiKey = '9cd9fac8c8e8487f90103d97de7b4140';
+
   constructor(private http: HttpClient) {}
 
   fetchNews(pageNum: number = 3, pageSize: number = 6): Observable<News[]> {
     return this.http
       .get<{ articles: News[] }>(
-        `http://newsapi.org/v2/everything?q=a&apiKey=9cd9fac8c8e8487f90103d97de7b4140&page=${pageNum}&pageSize=${pageSize}`
+        `http://newsapi.org/v2/everything?q=a&apiKey=${this.apiKey}&page=${pageNum}&pageSize=${pageSize}`
       )
       .pipe(pluck('articles'));
   }
