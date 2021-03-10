3f704bd4e5a28636e1da5f4e1a87d373a206307b
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-03-10 13:02:20 +0200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
 import { NewsContainerComponent } from './news-container/news-container.component';
 import { SingleNewComponent } from './news-container/single-new/single-new.component';
 import { PaginationComponent } from './pagination/pagination.component';
-import { FooterComponent } from './footer/footer.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +26,7 @@ import { FooterComponent } from './footer/footer.component';
     PaginationComponent,
     FormComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +39,8 @@ import { FooterComponent } from './footer/footer.component';
     ReactiveFormsModule,
     MatInputModule,
     MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
