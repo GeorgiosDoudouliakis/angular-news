a3e36c222096a040987c2e0fcefa6758aa80154c
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-03-29 23:20:11 +0300

    Add animation when the user navigates to main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,8 +1,24 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
+  animations: [
+    trigger('translateIn', [
+      state('in', style({ transform: 'translateY(0)', opacity: 1 })),
+      transition('void => *', [
+        style({ transform: 'translateY(-500px)', opacity: 0 }),
+        animate(700),
+      ]),
+    ]),
+  ],
 })
 export class MainPageComponent {}
