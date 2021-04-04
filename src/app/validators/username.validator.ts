a0049b3c948d3c48da0a688a80e2f758ca5b9fde
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-04-04 18:35:03 +0300

    Remove distinctUntilChanged operator in username validator

--- src/app/validators/username.validator.ts
+++ src/app/validators/username.validator.ts
@@ -4,14 +4,13 @@ import {
   ValidationErrors,
 } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, take } from 'rxjs/operators';
+import { debounceTime, map, take } from 'rxjs/operators';
 import { User } from '../models/user.model';
 
 export function forbiddenUsernameValidator(users: User[]): AsyncValidatorFn {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
     return control.valueChanges.pipe(
       debounceTime(600),
-      distinctUntilChanged(),
       take(1),
       map((res) => {
         const takenUsername = [];
