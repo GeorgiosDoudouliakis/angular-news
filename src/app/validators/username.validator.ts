46744f8227ecb7bd6f433ee525cca718da8bcfca
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-04-04 18:34:06 +0300

    Delete comments in username validator

--- src/app/validators/username.validator.ts
+++ src/app/validators/username.validator.ts
@@ -7,23 +7,6 @@ import { Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, take } from 'rxjs/operators';
 import { User } from '../models/user.model';
 
-// export function forbiddenUsernameValidator(
-//   usersService: UsersService
-// ): AsyncValidatorFn {
-//   return (control: AbstractControl): Observable<any> => {
-//     return usersService.getUsers().pipe(
-//       debounceTime(600),
-//       map((users: User[]) => {
-//         for (let user of users) {
-//           return user.username === control.value.username
-//             ? { usernameIsForbidden: true }
-//             : null;
-//         }
-//       })
-//     );
-//   };
-// }
-
 export function forbiddenUsernameValidator(users: User[]): AsyncValidatorFn {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
     return control.valueChanges.pipe(
